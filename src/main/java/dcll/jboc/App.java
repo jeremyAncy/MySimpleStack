3b591d2f3184f6120734b9a95b77f0016d63b010
jeremyAncy <bocchi31@gmail.com>
2015-03-06 08:28:09 +0100

    message de test 1

---
+++ src/main/java/dcll/jboc/App.java
@@ -0,0 +1,13 @@
+package dcll.jboc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
